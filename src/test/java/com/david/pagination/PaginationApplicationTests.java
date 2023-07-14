636057b5364af8f77ec4a3c138fac0b4ae4829fc
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2023-07-14 14:15:17 -0300

    Initial commit

---
+++ src/test/java/com/david/pagination/PaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.pagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
